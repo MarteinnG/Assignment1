edfb77f8829fc9992f1910fcdb1a61cb093cfc36
MarteinnG <101052414+MarteinnG@users.noreply.github.com>
2023-02-19 12:45:28 +0000

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Good morning!");
+            Console.Write("Write a number: ");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int number))
+                {
+                    numbers.Add(number);
+                    Console.Write("Write another number:");
+                }
+                else if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                else 
+                {
+                    Console.Write("Please enter a NUMBER, not a name or anything containing a letter or a sign: ");
+                }
+            }
+
+            if(numbers.Count == 0) 
+            {
+                Console.WriteLine("No numbers available");
+            } else
+            {
+                int lowest = numbers[0];
+                int highest = numbers[0];
+
+                foreach (int number in numbers) 
+                {
+                    if(number < lowest)
+                    {
+                        lowest = number;
+                    }
+                    if(number > highest)
+                    {
+                        highest = number;
+                    }
+                }
+
+                Console.WriteLine($"The lowest number: {lowest}");
+                Console.WriteLine($"The highest number: {highest}");
+            }
+            
+
+        }
+    }
+}
+
